9d762162d6c54aa5b86a6a8740f461efbe164520
SilverTaurus <139309096+SilverTaurus21@users.noreply.github.com>
2023-08-10 00:39:24 +0800

    Add files via upload
    This is Just a Sample UI using KRYPTON UI , Im still exploring about the capabilities of this 3rd party pre-requisites , it is good for startups who likes to design custom form. Im using C# as the main language for this. 
    
    I included the Krypton .dll located in Krypton Folder - also check this link on how to include krypton .dll in your system 
    ( https://www.youtube.com/watch?v=IF2HHacgjAU&t=2s ) hope this will help. Keep Exploring and Keep Learning!

---
+++ ZerothV1/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace ZerothV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BTN_MSG = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.sidebar.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.sidebar.Controls.Add(this.panel5);
+            this.sidebar.Controls.Add(this.panel1);
+            this.sidebar.Controls.Add(this.panel2);
+            this.sidebar.Controls.Add(this.panel3);
+            this.sidebar.Controls.Add(this.panel4);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.Location = new System.Drawing.Point(0, 0);
+            this.sidebar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.sidebar.MaximumSize = new System.Drawing.Size(213, 768);
+            this.sidebar.MinimumSize = new System.Drawing.Size(60, 661);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(213, 661);
+            this.sidebar.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Controls.Add(this.pictureBox1);
+            this.panel5.Location = new System.Drawing.Point(2, 3);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(228, 122);
+            this.panel5.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(85, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ZERO.v1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BTN_MSG);
+            this.panel1.Location = new System.Drawing.Point(2, 131);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(228, 77);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(2, 214);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(228, 77);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(2, 297);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(228, 77);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Location = new System.Drawing.Point(2, 380);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(228, 77);
+            this.panel4.TabIndex = 4;
+            // 
+            // sidebarTimer
+            // 
+            this.sidebarTimer.Interval = 5;
+            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZerothV1.Properties.Resources.alien48;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // BTN_MSG
+            // 
+            this.BTN_MSG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_MSG.Font = new System.Drawing.Font("Microsoft NeoGothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_MSG.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BTN_MSG.Image = global::ZerothV1.Properties.Resources.msg32;
+            this.BTN_MSG.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_MSG.Location = new System.Drawing.Point(-2, -7);
+            this.BTN_MSG.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.BTN_MSG.Name = "BTN_MSG";
+            this.BTN_MSG.Size = new System.Drawing.Size(236, 99);
+            this.BTN_MSG.TabIndex = 0;
+            this.BTN_MSG.Text = "Mail";
+            this.BTN_MSG.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft NeoGothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Image = global::ZerothV1.Properties.Resources.location32;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(-3, -12);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "    Location";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft NeoGothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Image = global::ZerothV1.Properties.Resources.addlist32;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(-2, -12);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 99);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "    Inventory";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft NeoGothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Image = global::ZerothV1.Properties.Resources.database32;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(-2, -12);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(253, 99);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "  Back-up";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panelTop);
+            this.flowLayoutPanel1.Controls.Add(this.panel8);
+            this.flowLayoutPanel1.Controls.Add(this.panel9);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(213, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1001, 661);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(3, 127);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(998, 484);
+            this.panel8.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(3, 617);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(998, 33);
+            this.panel9.TabIndex = 3;
+            // 
+            // panelTop
+            // 
+            this.panelTop.Location = new System.Drawing.Point(3, 3);
+            this.panelTop.MaximumSize = new System.Drawing.Size(1230, 115);
+            this.panelTop.MinimumSize = new System.Drawing.Size(998, 115);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(998, 115);
+            this.panelTop.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1214, 661);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.sidebar);
+            this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2010Black;
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StateActive.Back.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Back.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Border.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Border.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateActive.Border.Rounding = 5;
+            this.StateActive.Header.Back.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Header.Back.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Header.Border.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Header.Border.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateActive.Header.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Back.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Back.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Border.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Border.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.Rounding = 10;
+            this.StateCommon.Header.Back.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Header.Back.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Header.Border.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Header.Border.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateCommon.Header.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateInactive.Back.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Back.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Border.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Border.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateInactive.Header.Back.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Header.Back.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Header.Border.Color1 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Header.Border.Color2 = System.Drawing.Color.DarkSlateBlue;
+            this.StateInactive.Header.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Text = "Zero V.1";
+            this.sidebar.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Button BTN_MSG;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer sidebarTimer;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panel9;
+    }
+}
+
---
+++ ZerothV1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace ZerothV1
+{
+    public partial class Form1 : KryptonForm /// change to kyrpton
+    {
+        bool sidebarExpand;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sidebarTimer_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width == sidebar.MinimumSize.Width)
+                {
+                 
+                    panelTop.Size = new Size(1228,115);
+                    sidebarExpand = false;
+                    sidebarTimer.Stop();
+                }
+
+            }
+
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    
+                  
+                    panelTop.Size = new Size(998, 115);
+                    sidebarExpand = true;
+                    sidebarTimer.Stop();
+                }
+
+
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            sidebarTimer.Start();
+        }
+    }
+}
